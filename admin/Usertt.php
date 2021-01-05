996e9d7bbdd5b7f0c8f5c157a5b530bc52383a4e
Anand Jaiswal <anand.grh@gmail.com>
2021-01-05 18:19:46 +0530

    Update Usertt.php
    Adding Time Login option.

--- admin/Usertt.php
+++ admin/Usertt.php
@@ -1,14 +1,28 @@
-	<!-- Enable Time Login-->
-<?php 
-  ?>
-      <tr class="user-display-time-login">
-      <th><label for="display_login"><?php _e( 'Enable Time Login :' ); ?></label></th>
-      <td>
-          <select name="display_login" id="display_login">
+<?php
+/**
+ * Show custom user profile fields
+ * Add Time Login in Customer Login page.
+ * @param  object $profileuser A WP_User object
+ * @return void
+ */
+
+function custom_user_profile_fields( $profileuser ) {
+?>
+    <table class="form-table">
+        <tr>
+            <th>
+                <label for="user_location"><?php esc_html_e( 'Enable Time Login :' ); ?></label>
+            </th>
+            <td>
+            <select name="display_login" id="display_login">
           <option value="True">True</option>
           <option value="False">False</option>
           </select>
-          </td>
-      </tr>
-    <?php 
-?>
+            </td>
+        </tr>
+    </table>
+<?php
+}
+
+add_action( 'wp_create_application_password_form', 'custom_user_profile_fields', 10, 1 );
+
