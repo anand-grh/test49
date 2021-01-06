2e319e284496f2929726d30173db0c767c57a658
unknown <Anandkumar.Jaiswal@INDOLINE.LOCAL>
2021-01-06 18:05:20 +0530

    Update Usertt.php
    Store Tracking True or False In Database

--- admin/Usertt.php
+++ admin/Usertt.php
@@ -14,9 +14,18 @@ function custom_user_profile_fields( $profileuser ) {
                 <label for="user_location"><?php esc_html_e( 'Enable Time Login :' ); ?></label>
             </th>
             <td>
-            <select name="display_login" id="display_login">
+            <select name="Tracker" id="Tracker">
           <option value="True">True</option>
           <option value="False">False</option>
+          <?php
+			$public_display['Tracker'] = $profileuser->Tracker;
+
+		foreach ( $public_display as $id => $item ) {
+			?>
+		<option <?php selected( $profileuser->Tracker, $item ); ?> ><?php echo $item; ?></option>
+			<?php
+		}
+		?>
           </select>
             </td>
         </tr>
@@ -24,5 +33,15 @@ function custom_user_profile_fields( $profileuser ) {
 <?php
 }
 
+function save_extra_user_profile_fields( $user_id ) {
+    if ( !current_user_can( 'edit_user', $user_id ) ) { 
+        return false; 
+    }
+    update_user_meta( $user_id, 'Tracker', $_POST['Tracker'] );
+}
+
+
 add_action( 'wp_create_application_password_form', 'custom_user_profile_fields', 10, 1 );
+add_action( 'personal_options_update', 'save_extra_user_profile_fields' );
+add_action( 'edit_user_profile_update', 'save_extra_user_profile_fields' );
 
